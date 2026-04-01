26bc83a1e4545ef30ea70b1eb104764e3569c4df
NimaYoezer28 <nima.yoezer@ecentric.ngn.bt>
2026-04-01 12:02:29 +0600

    changes in various services in mining project

---
+++ src/main/java/com/mas/gov/bt/mas/primary/dto/request/ReAssignedTaskMI.java
@@ -0,0 +1,12 @@
+package com.mas.gov.bt.mas.primary.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class ReAssignedTaskMI {
+    private Long Id;
+    private Long MIFocalId;
+    private String remarksMI;
+}
