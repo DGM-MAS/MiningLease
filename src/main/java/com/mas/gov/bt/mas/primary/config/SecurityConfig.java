1fc9e7164a6abadc05dcb94279335e0b1e00ff0c
GaneshChhetri <ganesh.chhetri@ecentric.ngn.bt>
2026-04-20 11:25:07 +0600

    CORs removed

--- src/main/java/com/mas/gov/bt/mas/primary/config/SecurityConfig.java
+++ src/main/java/com/mas/gov/bt/mas/primary/config/SecurityConfig.java
@@ -24,7 +24,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .cors(cors -> cors.disable())
             .csrf(csrf -> csrf.disable())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
