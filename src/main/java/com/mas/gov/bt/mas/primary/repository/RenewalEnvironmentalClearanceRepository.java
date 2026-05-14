8fc2c92ffd3047aa8ac68896b2895590c6c79811
NimaYoezer28 <nima.yoezer@ecentric.ngn.bt>
2026-05-14 09:20:24 +0600

    changes from multiple service are being pushed

---
+++ src/main/java/com/mas/gov/bt/mas/primary/repository/RenewalEnvironmentalClearanceRepository.java
@@ -0,0 +1,152 @@
+package com.mas.gov.bt.mas.primary.repository;
+
+import com.mas.gov.bt.mas.primary.dto.UserWorkloadProjection;
+import com.mas.gov.bt.mas.primary.entity.EnvironmentClearanceRenewal;
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface RenewalEnvironmentalClearanceRepository extends JpaRepository<EnvironmentClearanceRenewal, Long> {
+    @Query("""
+       SELECT MAX(
+           CAST(
+               SUBSTRING(
+                   e.applicationNo,
+                   LENGTH(:prefix) + 1
+               ) AS integer
+           )
+       )
+       FROM EnvironmentClearanceRenewal e
+       WHERE e.applicationNo LIKE CONCAT(:prefix, '%')
+       """)
+    Integer findMaxSequenceByPrefix(
+            @Param("prefix") String prefix
+    );
+
+    @Query(value = """
+    SELECT
+        u.id AS userId,
+        u.email AS email,
+        u.username AS userName,
+        COUNT(u.id) AS workload
+    FROM mas_db.users u
+    JOIN mas_db.user_roles ur
+        ON u.id = ur.user_id
+    LEFT JOIN mas_db.role_permissions t
+        ON t.role_id = ur.role_id
+    WHERE ur.role_id = 21
+      AND t.permission_id = 37
+      AND u.account_status = 'ACTIVE'
+    GROUP BY u.id, u.email, u.username
+    ORDER BY workload ASC
+    LIMIT 1
+    """, nativeQuery = true)
+    UserWorkloadProjection findMDEnvironmentalClearance();
+
+    @Query(value = """
+    SELECT
+        u.id AS userId,
+        u.email AS email,
+        u.username AS userName,
+        COUNT(u.id) AS workload
+    FROM mas_db.users u
+    JOIN mas_db.user_roles ur
+        ON u.id = ur.user_id
+    LEFT JOIN mas_db.role_permissions t
+        ON t.role_id = ur.role_id
+    WHERE ur.role_id = 21
+      AND t.permission_id = 37
+      AND u.account_status = 'ACTIVE'
+    GROUP BY u.id, u.email, u.username
+    ORDER BY workload ASC
+    LIMIT 1
+    """, nativeQuery = true)
+    UserWorkloadProjection findMPCDEnvironmentalClearance();
+
+
+    @Query(value = """
+    SELECT 
+        u.id AS userId,
+        u.email AS email,
+        u.username AS userName
+    FROM mas_db.users u
+    WHERE u.id = :directorId
+      AND u.account_status = 'ACTIVE'
+    GROUP BY u.id, u.email, u.username
+    LIMIT 1
+    """, nativeQuery = true)
+    UserWorkloadProjection findUserDetails(Long directorId);
+
+    Page<EnvironmentClearanceRenewal> findByAssignedMPCDId(Long userId, Pageable pageable);
+
+    Page<EnvironmentClearanceRenewal> findByAssignedMPCDIdAndApplicationNoContainingIgnoreCase(Long userId, String search, Pageable pageable);
+
+    @Query("SELECT a FROM EnvironmentClearanceRenewal a WHERE a.status IN :applicationStatus AND a.createdBy = :userId ORDER BY a.createdOn DESC")
+    Page<EnvironmentClearanceRenewal> findByApplicantUserIdAndStatusIn(Long userId, List<String> applicationStatus, Pageable pageable);
+
+    @Query("""
+    SELECT q FROM EnvironmentClearanceRenewal q
+    WHERE q.createdBy = :userId
+    AND q.status IN :applicationStatus
+    AND LOWER(q.applicationNo) LIKE LOWER(CONCAT('%', :trim, '%'))
+""")
+    Page<EnvironmentClearanceRenewal> findByAssignedToUserAndSearch(Long userId, List<String> applicationStatus, String trim, Pageable pageable);
+
+    Page<EnvironmentClearanceRenewal> findByStatusIn(List<String> approved, Pageable pageable);
+
+    Page<EnvironmentClearanceRenewal> findByStatusInAndApplicationNoContainingIgnoreCase(List<String> approved, String search, Pageable pageable);
+
+    Page<EnvironmentClearanceRenewal> findByCreatedByAndStatusIn(Long userId, List<String> approved, Pageable pageable);
+
+    Page<EnvironmentClearanceRenewal> findByCreatedByAndStatusInAndApplicationNoContainingIgnoreCase(Long userId, List<String> approved, String search, Pageable pageable);
+
+    Optional<EnvironmentClearanceRenewal> findByIdAndCreatedBy(Long id, Long userId);
+
+    Optional<EnvironmentClearanceRenewal> findByApplicationNo(String applicationNumber);
+
+    Page<EnvironmentClearanceRenewal> findByAssignedMPCDIdAndStatusIn(Long userId, List<String> archivedStatuses, Pageable pageable);
+
+    Page<EnvironmentClearanceRenewal> findByAssignedMPCDIdAndApplicationNoContainingIgnoreCaseAndStatusIn(Long userId, String trim, List<String> archivedStatuses, Pageable pageable);
+
+    Page<EnvironmentClearanceRenewal>
+    findByAssignedRCId(
+            Long userId,
+            Pageable pageable
+    );
+
+    Page<EnvironmentClearanceRenewal>
+    findByAssignedRCIdAndApplicationNoContainingIgnoreCase(
+            Long userId,
+            String search,
+            Pageable pageable
+    );
+
+    Page<EnvironmentClearanceRenewal> findByAssignedRCIdAndStatusIn(Long userId, List<String> applicationStatuses, Pageable pageable);
+
+    Page<EnvironmentClearanceRenewal> findByAssignedRCIdAndApplicationNoContainingIgnoreCaseAndStatusIn(Long userId, String trim, List<String> applicationStatuses, Pageable pageable);
+
+    Page<EnvironmentClearanceRenewal> findByAssignedMIIdAndStatusIn(Long userId, List<String> applicationStatuses, Pageable pageable);
+
+    Page<EnvironmentClearanceRenewal> findByAssignedMIIdAndApplicationNoContainingIgnoreCaseAndStatusIn(Long userId, String trim, List<String> applicationStatuses, Pageable pageable);
+
+    Page<EnvironmentClearanceRenewal>
+    findByAssignedMDIdAndStatusIn(
+            Long userId,
+            List<String> status,
+            Pageable pageable
+    );
+
+    Page<EnvironmentClearanceRenewal>
+    findByAssignedMDIdAndApplicationNoContainingIgnoreCaseAndStatusIn(
+            Long userId,
+            String search,
+            List<String> status,
+            Pageable pageable
+    );
+}
--- src/main/java/com/mas/gov/bt/mas/primary/services/SurfaceCollectionAuctionServiceImpl.java
+++ src/main/java/com/mas/gov/bt/mas/primary/services/SurfaceCollectionAuctionServiceImpl.java
@@ -2,15 +2,23 @@ package com.mas.gov.bt.mas.primary.services;
 
 import com.mas.gov.bt.mas.primary.dto.request.BGRequestDTO;
 import com.mas.gov.bt.mas.primary.dto.request.BidWinnerRequestDTO;
+import com.mas.gov.bt.mas.primary.dto.request.SurfaceCollectionAuctionListResponseDTO;
 import com.mas.gov.bt.mas.primary.dto.request.SurfaceCollectionAuctionRequestDTO;
+import com.mas.gov.bt.mas.primary.dto.response.SurfaceCollectionAttachmentResponseDTO;
 import com.mas.gov.bt.mas.primary.dto.response.SurfaceCollectionAuctionResponseDTO;
+import com.mas.gov.bt.mas.primary.entity.SurfaceCollectionAttachment;
 import com.mas.gov.bt.mas.primary.entity.SurfaceCollectionAuctionApplication;
 import com.mas.gov.bt.mas.primary.entity.SurfaceCollectionBidWinner;
+import com.mas.gov.bt.mas.primary.repository.SurfaceCollectionAttachmentRepository;
 import com.mas.gov.bt.mas.primary.repository.SurfaceCollectionAuctionRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.time.Year;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -18,6 +26,8 @@ import java.util.UUID;
 public class SurfaceCollectionAuctionServiceImpl implements SurfaceCollectionAuctionService {
 
     private final SurfaceCollectionAuctionRepository auctionRepository;
+
+    private final SurfaceCollectionAttachmentRepository surfaceCollectionAttachmentRepository;
 
     @Override
     public SurfaceCollectionAuctionResponseDTO createAuction(
@@ -31,10 +41,23 @@ public class SurfaceCollectionAuctionServiceImpl implements SurfaceCollectionAuc
                         .location(dto.getLocation())
                         .area(dto.getArea())
                         .material(dto.getMaterial())
-                        .auctionStatus("DRAFT")
+                        .auctionStatus("SUBMITTED")
                         .createdBy(userId)
                         .createdOn(LocalDateTime.now())
                         .build();
+
+
+        List<SurfaceCollectionAttachment> attachments =
+                dto.getAttachments()
+                        .stream()
+                        .map(att -> SurfaceCollectionAttachment.builder()
+                                .attachmentType(att.getAttachmentType())
+                                .uploadFileId(att.getFileId())
+                                .auctionApplication(entity)
+                                .build())
+                        .toList();
+
+        entity.setAttachments(attachments);
 
         auctionRepository.save(entity);
 
@@ -161,7 +184,14 @@ public class SurfaceCollectionAuctionServiceImpl implements SurfaceCollectionAuc
     }
 
     private String generateApplicationNo() {
-        return "SCA-" + UUID.randomUUID().toString().substring(0, 8);
+        int year = Year.now().getValue();
+        String prefix = String.format("SCA-%d-", year);
+
+        // Get max sequence from database for current year
+        Integer maxSequence = auctionRepository.findMaxSequenceByPrefix(prefix);
+        long nextSequence = (maxSequence == null ? 0 : maxSequence) + 1;
+
+        return String.format("SCA-%d-%06d", year, nextSequence);
     }
 
     private SurfaceCollectionAuctionResponseDTO mapToResponse(
@@ -178,6 +208,96 @@ public class SurfaceCollectionAuctionServiceImpl implements SurfaceCollectionAuc
                 .auctionStatus(entity.getAuctionStatus())
                 .submittedForEc(entity.getSubmittedForEc())
                 .submittedForFc(entity.getSubmittedForFc())
+                .bgRequested(entity.getBgRequested())
+                .permitGenerated(entity.getPermitGenerated())
+                .createdOn(entity.getCreatedOn())
+                .build();
+    }
+
+    @Override
+    public Page<SurfaceCollectionAuctionListResponseDTO> getAllApplications(
+            String search,
+            Pageable pageable) {
+
+        Page<SurfaceCollectionAuctionApplication> page;
+
+        if (search == null || search.isBlank()) {
+
+            page = auctionRepository.findAll(pageable);
+
+        } else {
+
+            page = auctionRepository
+                    .findByApplicationNoContainingIgnoreCaseOrLocationContainingIgnoreCase(
+                            search,
+                            search,
+                            pageable
+                    );
+        }
+
+        return page.map(this::mapListResponse);
+    }
+
+    @Override
+    public List<SurfaceCollectionAttachmentResponseDTO>
+    getAttachmentsByAuctionId(Long auctionId) {
+
+        List<SurfaceCollectionAttachment> attachments =
+                surfaceCollectionAttachmentRepository.findByAuctionApplicationId(auctionId);
+
+        return attachments.stream()
+                .map(this::mapAttachment)
+                .toList();
+    }
+
+    @Override
+    public Page<SurfaceCollectionAuctionListResponseDTO> getMyApplications(String search, Pageable pageable, Long userId) {
+        Page<SurfaceCollectionAuctionApplication> page;
+
+        List<String> archivedStatuses = List.of("SUBMITTED");
+
+        if (search == null || search.isBlank()) {
+
+            page = auctionRepository.findByCreatedByAndAuctionStatusIn(userId,archivedStatuses,pageable);
+
+        } else {
+
+            page = auctionRepository
+                    .findByApplicationNoContainingIgnoreCaseOrLocationContainingIgnoreCaseAndAuctionStatusIn(
+                            search,
+                            search,
+                            archivedStatuses,
+                            pageable
+                    );
+        }
+
+        return page.map(this::mapListResponse);
+    }
+
+    private SurfaceCollectionAttachmentResponseDTO mapAttachment(
+            SurfaceCollectionAttachment attachment
+    ) {
+
+        return SurfaceCollectionAttachmentResponseDTO.builder()
+                .id(attachment.getId())
+                .attachmentType(attachment.getAttachmentType())
+                .uploadFileId(attachment.getUploadFileId())
+                .build();
+    }
+
+    private SurfaceCollectionAuctionListResponseDTO mapListResponse(
+            SurfaceCollectionAuctionApplication entity
+    ) {
+
+        return SurfaceCollectionAuctionListResponseDTO.builder()
+                .id(entity.getId())
+                .applicationNo(entity.getApplicationNo())
+                .location(entity.getLocation())
+                .area(entity.getArea())
+                .material(entity.getMaterial())
+                .ecStatus(entity.getEcStatus())
+                .fcStatus(entity.getFcStatus())
+                .auctionStatus(entity.getAuctionStatus())
                 .bgRequested(entity.getBgRequested())
                 .permitGenerated(entity.getPermitGenerated())
                 .createdOn(entity.getCreatedOn())
