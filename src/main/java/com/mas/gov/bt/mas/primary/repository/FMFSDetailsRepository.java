9176560438422ba2313e7e340e8a21a8a4ea08cd
GaneshChhetri <ganesh.chhetri@ecentric.ngn.bt>
2026-08-01 21:46:48 +0600

    resolve conflict with upstream mining lease reserve fields

--- src/main/java/com/mas/gov/bt/mas/primary/repository/FMFSDetailsRepository.java
+++ src/main/java/com/mas/gov/bt/mas/primary/repository/FMFSDetailsRepository.java
@@ -3,5 +3,9 @@ package com.mas.gov.bt.mas.primary.repository;
 import com.mas.gov.bt.mas.primary.entity.FmfsDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FMFSDetailsRepository extends JpaRepository<FmfsDetails, Long> {
+
+    Optional<FmfsDetails> findFirstByApplicationNumberOrderByUpdatedAtDescIdDesc(String applicationNumber);
 }
